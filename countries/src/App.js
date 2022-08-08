7e6677c3d980e08840dadf625f91afd982d3e3e2
Ali-Chimim <65404466+Ali-Chimim@users.noreply.github.com>
2022-08-08 17:35:52 -0400

    added font and navbarr

--- countries/src/App.js
+++ countries/src/App.js
@@ -1,24 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import * as React from "react";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import DarkModeOutlinedIcon from "@mui/icons-material/DarkModeOutlined";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+
+const theme = createTheme({
+  typography: {
+    fontFamily: ["Outfit", "cursive"].join(","),
+  },
+});
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <ThemeProvider theme={theme}>
+      <Box sx={{ flexGrow: 1 }}>
+        <AppBar
+          color="transparent"
+          position="static"
+          sx={{ padding: "0px 15px" }}
         >
-          Learn React
-        </a>
-      </header>
-    </div>
+          <Toolbar>
+            <Typography
+              variant="subtitle1"
+              component="div"
+              sx={{ flexGrow: 1, fontWeight: 700 }}
+            >
+              Where in the world ?
+            </Typography>
+            <IconButton>
+              <DarkModeOutlinedIcon
+                sx={{
+                  marginRight: "5px",
+                  width: "17px",
+                  height: "17px",
+                  color: "black",
+                }}
+              />
+              <Typography
+                variant="subtitle2"
+                component="div"
+                sx={{ flexGrow: 1, color: "black" }}
+              >
+                Dark Mode
+              </Typography>
+            </IconButton>
+          </Toolbar>
+        </AppBar>
+      </Box>
+    </ThemeProvider>
   );
 }
 
