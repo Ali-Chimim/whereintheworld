4bf4b10bbba083ca6024a10805e3a268e000d4a7
Ali-Chimim <65404466+Ali-Chimim@users.noreply.github.com>
2022-08-12 16:10:47 -0400

    added axios + exemple of card

--- my-app/src/pages/HomePage.tsx
+++ my-app/src/pages/HomePage.tsx
@@ -1,6 +1,7 @@
-import { Grid } from "@mui/material";
+import { Card, CardContent, Grid, Typography } from "@mui/material";
 import FilterMenu from "../components/FilterMenu";
 import SearchInput from "../components/SearchInput";
+import MoroccanFlag from "../assets/images/Flag_of_Morocco.svg.png";
 
 const HomePage = () => {
   return (
@@ -24,6 +25,57 @@ const HomePage = () => {
         <SearchInput />
         <FilterMenu />
       </Grid>
+      <Card
+        variant="outlined"
+        sx={{
+          width: "240px",
+          borderRadius: "5px",
+          boxShadow: " 0 0 15px rgb(0 0 0 /0.15);",
+          border: 0,
+          cursor: "pointer",
+          transition: "transform 250ms",
+          "&:hover": { transform: "translateY(-10px)" },
+        }}
+      >
+        <CardContent
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            padding: 0,
+          }}
+        >
+          <img
+            src={MoroccanFlag}
+            style={{
+              width: "100%",
+              borderRadius: "5px 5px 0px 0px ",
+              marginBottom: "20px",
+            }}
+            alt="flag"
+          />
+          <Typography
+            style={{
+              fontWeight: 700,
+              marginBottom: "10px",
+              fontSize: "14px",
+            }}
+          >
+            Morocco
+          </Typography>
+          <Typography
+            variant="caption"
+            style={{
+              color: "grey",
+              marginBottom: "5px",
+            }}
+          >
+            Scan the QR code to visit Frontend Mentor and take your coding
+            skills to the next level
+          </Typography>
+        </CardContent>
+      </Card>
+
       {/* Container of the cards */}
     </Grid>
   );
